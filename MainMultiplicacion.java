7f1c57ef78f5f099c07c1cc9caee938ff1eeb073
Mairim <nygmaurie@gmail.com>
2024-05-06 22:10:26 -0600

    version 11

--- MainMultiplicacion.java
+++ MainMultiplicacion.java
@@ -1,19 +1,16 @@
-import java.util.Scanner;
-public class MainMultiplicacion {
-    public static void main(String[] args) {
-        Multiplicacion multiplicacion1 = new Multiplicacion();
-        int a,e;
+import java.util.Scanner;//El código importa la clase Scanner del paquete java.util, que se usa para leer la entrada del usuario. 
+public class MainMultiplicacion {//Se declara una clase llamada MainMultiplicacion.
+    public static void main(String[] args) {//Se define el método principal main, que es el punto de entrada del programa.
+        Multiplicacion multiplicacion1 = new Multiplicacion();//Se crea un objeto multiplicacion1 de la clase Multiplicacion.
+        int a,e;//Se declaran las variables a y e de tipo entero.
         
-        Scanner op = new Scanner(System.in);
-        System.out.print("Ingresa el primer numero: ");
+        Scanner op = new Scanner(System.in);//Se crea un objeto op de la clase Scanner para leer la entrada del usuario.
+        System.out.print("Ingresa el primer numero: ");//Se solicita al usuario que ingrese dos números (a y e) utilizando nextInt() para leer enteros.
         a = op.nextInt();
         System.out.print("Ingresa el segundo numero: ");
         e = op.nextInt();
-
-        multiplicacion1.num1 = a;
-        multiplicacion1.num2 = e;
-
-        multiplicacion1.MostrarResultado();
-
+        multiplicacion1.setNum1(a);//Los valores ingresados por el usuario (a y e) se asignan a los objetos multiplicacion1 utilizando los métodos setNum1 y setNum2 de la clase Multiplicacion.
+        multiplicacion1.setNum2(e);
+        multiplicacion1.MostrarResultado();//Se llama al método MostrarResultado() del objeto multiplicacion1 para mostrar el resultado de la multiplicación.
     }
 }
--- Multiplicacion.java
+++ Multiplicacion.java
@@ -1,11 +1,26 @@
-class Multiplicacion{
-    int num1, num2;
-    Multiplicacion(){
+class Multiplicacion{ //Se declara la clase Multiplicacion, que contiene métodos y variables para realizar operaciones de multiplicación.
+    int num1, num2;  //Se declaran dos variables enteras
+    Multiplicacion(){ //Se define el constructor de la clase Multiplicacion, que imprime un mensaje cuando se crea una instancia de la clase.
         System.out.println("SE ESTA CONSTRUYENDO LA MULTIPLICACION");
     }
-
-    public void MostrarResultado(){
+    public void MostrarResultado(){//Este método imprime el resultado de la multiplicación de num1 por num2.
         System.out.println("El resultado es " + (this.num1 * this.num2));
+    }
+    public int getNum1() {//Estos métodos permiten obtener y establecer los valores de num1 y num2. Proporcionan encapsulación para acceder a las variables de la clase.
+        return num1;
+    }
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+    public int getNum2() {
+        return num2;
+    }
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+    @Override
+    public String toString() {//Este método devuelve una representación de cadena de la instancia de la clase Multiplicacion, mostrando los valores de num1 y num2.
+        return "Multiplicacion [num1=" + num1 + ", num2=" + num2 + "]";
     }
         
 }
